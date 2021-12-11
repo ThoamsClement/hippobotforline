186c2c74b96cd27388e5ed93c672d97b09997df9
iamhippo <hippoiam@icloud.com>
2021-12-11 23:21:55 +0800

    TEST

--- LineBot/settings.py
+++ LineBot/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'line_bot'
+    'line_bot.apps.line_botConfig'
 ]
 
 MIDDLEWARE = [
